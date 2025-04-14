80350d1818ada89afe8b5ee0e40446f1bba86908
Ramzi Sleiman <ramzisleiman@Ramzis-MacBook-Air.local>
2025-04-14 11:34:24 +0300

    Initial commit from Create Next App

---
+++ src/pages/_app.tsx
@@ -0,0 +1,6 @@
+import "npx create-next-app@latest . --typescript@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
